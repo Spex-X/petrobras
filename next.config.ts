00353590f4ab3220aaebf7a8b35064674593f246
jhonne333332 <seu-email@example.com>
2026-02-25 01:33:22 -0400

    fix: configure next.js for static export to resolve vercel 404

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  trailingSlash: true,
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
